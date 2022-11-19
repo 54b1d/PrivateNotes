f2be0f070269ff4bcaaa7be8d000f147b7086150
54b1d <sabidul1@gmail.com>
2022-11-19 22:59:17 +0600

    Initial commit

---
+++ app/src/main/java/com/sabid/privatenotes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sabid.privatenotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
